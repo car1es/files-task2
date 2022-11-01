e02a293f3671965dfc503b866e96ce4134ba8d6c
Alexey Gorokhov <car1es@bk.ru>
2022-11-01 16:26:49 +0300

    change zipfiles(), add deleteFiles()

--- Main.java
+++ Main.java
@@ -8,17 +8,20 @@ import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static void main(String[] args) {
+        String path = "Games/savegames/";
         List<String> listFiles = new ArrayList<>();
-        listFiles.add("Games/savegames/save1.dat");
-        listFiles.add("Games/savegames/save2.dat");
-        listFiles.add("Games/savegames/save3.dat");
+        listFiles.add(path + "save1.dat");
+        listFiles.add(path + "save2.dat");
+        listFiles.add(path + "save3.dat");
         GameProgress gp1 = new GameProgress(100, 40, 3, 50.3);
         GameProgress gp2 = new GameProgress(80, 90, 9, 99.3);
         GameProgress gp3 = new GameProgress(65, 85, 6, 70);
-        saveGame("Games/savegames/save1.dat", gp1);
-        saveGame("Games/savegames/save2.dat", gp2);
-        saveGame("Games/savegames/save3.dat", gp3);
-        zipFiles("Games/savegames/zip.zip", listFiles);
+        saveGame(path + "save1.dat", gp1);
+        saveGame(path + "save2.dat", gp2);
+        saveGame(path + "save3.dat", gp3);
+        zipFiles(path + "backup.zip", listFiles);
+        deleteFiles(path);
+
 
     }
 
@@ -33,9 +36,9 @@ public class Main {
     }
 
     public static void zipFiles(String path, List<String> files) {
-        try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path))){
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path))) {
             for (int i = 0; i < files.size(); i++) {
-                try(FileInputStream fis = new FileInputStream(files.get(i))){
+                try (FileInputStream fis = new FileInputStream(files.get(i))) {
                     ZipEntry entry = new ZipEntry(files.get(i));
                     zout.putNextEntry(entry);
                     byte[] buffer = new byte[fis.available()];
@@ -49,12 +52,17 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        File dir = new File("Games/savegames/");
-        for (File file : dir.listFiles()){
-            if (!file.getName().endsWith(".zip")){
+    public static boolean deleteFiles(String path) {
+        boolean result = false;
+        File dir = new File(path);
+        for (File file : dir.listFiles()) {
+            if (!file.getName().endsWith(".zip")) {
                 file.delete();
+                result = true;
             }
         }
+        return result;
     }
 }
